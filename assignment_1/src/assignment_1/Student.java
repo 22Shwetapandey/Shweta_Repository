741be979579be8f41bfc736478657ce939fe262c
tript <tript@LAPTOP-LPHEO3NN>
2022-04-23 00:57:03 +0530

    Pushing the code first time.

---
+++ assignment_1/src/assignment_1/Student.java
@@ -0,0 +1,25 @@
+package assignment_1;
+
+public class Student {
+int age;
+int roll_no;
+public void display1()
+{
+System.out.println("Welcome to all of you");	
+}
+public void display2()
+{
+System.out.println("Automation is easy");
+}
+public static void main(String[] args) {
+	Student so = new Student();
+	so.display1();
+	so.display2();
+	so.age=20;
+	so.roll_no=13;
+	System.out.println("Student age is:" +so.age);
+	System.out.println("Student roll no is:" +so.roll_no);
+			
+}
+
+}
